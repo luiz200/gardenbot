6e3c14b955fe2eb52278826ce894260692485ba0
luizs <luiz.souza.709@ufrn.edu.br>
2024-02-11 23:24:06 -0300

    #9 feat: finalizado a leitura e publicação

--- hardware/app/app.ino
+++ hardware/app/app.ino
@@ -4,6 +4,7 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/semphr.h>
+#include <DHT.h>
 
 //Sensores de umidade solo
 
@@ -14,32 +15,24 @@ byte solo4 = 35;
 
 //Motores de passo
 
-byte motor1 = 25;
-byte motor2 = 26;
-byte motor3 = 27;
-
-//Eletroimã
-
-byte ima1 = 12;
-byte ima2 = 13;
-byte ima3 = 14;
-byte ima4 = 23;
-
-//Bomba
-
-byte bomba = 22;
-
-//Micro switcher
-
-byte switcher = 21;
-
-//Temperatura
-
-byte temp = 34;
-
-//Umidade do ar
-
-byte ar = 18;
+//byte motor1 = 25;
+//byte motor2 = 26;
+//byte motor3 = 27;
+//
+////Eletroimã
+//
+//byte ima1 = 12;
+//byte ima2 = 13;
+//byte ima3 = 14;
+//byte ima4 = 23;
+//
+////Bomba
+//
+//byte bomba = 22;
+//
+////Micro switcher
+//
+//byte switcher = 21;
 
 // WiFi
 const char *ssid = "Simone";           // Enter your WiFi name
@@ -60,55 +53,37 @@ const char *mqtt_password = "12345";
 const int mqtt_port = 1883;
 
 
-float temperature;
+float temp, humi;
 
-int tempValue, solo1Value, solo2Value, solo3Value, solo4Value, solo1Porcentagem, solo2Porcentagem, solo3Porcentagem, solo4Porcentagem;
+int solo1Value, solo2Value, solo3Value, solo4Value, solo1Porcentagem, solo2Porcentagem, solo3Porcentagem, solo4Porcentagem;
 
-String temperatureStr, solo1PorcentagemStr, solo2PorcentagemStr, solo3PorcentagemStr, solo4PorcentagemStr;
-
-const int termistorPin = temp;         // Pino analógico onde o termistor está conectado
-const float termistorNominal = 10000;  // Resistência nominal do termistor
-const float tempNominal = 25.0;        // Temperatura nominal do termistor em graus Celsius
-const float beta = 3950;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+DHT dht(4, DHT22);
+
 SemaphoreHandle_t xSemaphore;
 
-float convertToTemperature(int termistorValue) {
-  const float seriesResistor = 10000.0;
-  const float termistorNominal = 10000.0;
-  const float termistorCoefficient = 3950.0;
-  const float termistorTemperature = 25.0;
-
-  float voltage = termistorValue * (3.3 / 4095.0);
-  float resistance = (3.3 * seriesResistor / voltage) - seriesResistor;
-  float temperature = (resistance - termistorNominal) / termistorCoefficient;
-  temperature += termistorTemperature;
-
-  return temperature;
-}
-
-void ledTask(void *parameter){
-  while (1) {
-    //ldrValue = analogRead(ldr);
-    tempValue = analogRead(temp);
-    //ldrPercentage = map(ldrValue, 0, 4095, 100, 0);
-    temperature = convertToTemperature(tempValue);
-    // receber do broker do topico temperatura
-    // if (ldrPercentage < 20) {
-    //   digitalWrite(led, 1);
-    // } else {
-    //   digitalWrite(led, 0);
-    // }
-    // if (temperature > 27) {
-    //   dacWrite(cooler, 4095);
-    // } else {
-    //   dacWrite(cooler, 0);
-    // }
-  }
-}
+//void ledTask(void *parameter){
+//  while (1) {
+//    //ldrValue = analogRead(ldr);
+//    tempValue = analogRead(temp);
+//    //ldrPercentage = map(ldrValue, 0, 4095, 100, 0);
+//    temperature = convertToTemperature(tempValue);
+//    // receber do broker do topico temperatura
+//    // if (ldrPercentage < 20) {
+//    //   digitalWrite(led, 1);
+//    // } else {
+//    //   digitalWrite(led, 0);
+//    // }
+//    // if (temperature > 27) {
+//    //   dacWrite(cooler, 4095);
+//    // } else {
+//    //   dacWrite(cooler, 0);
+//    // }
+//  }
+//}
 
 void connectWiFiTask(void *parameter) {
   WiFi.begin(ssid, password);
@@ -138,7 +113,9 @@ void mqttTask(void *parameter) {
   while (1) {
     client.loop();
       // publish and subscribe
-      tempValue = analogRead(temp);
+      temp = dht.readTemperature();
+      humi = dht.readHumidity();
+      
       solo1Value = analogRead(solo1);
       solo2Value = analogRead(solo2);
       solo3Value = analogRead(solo3);
@@ -149,24 +126,14 @@ void mqttTask(void *parameter) {
       solo3Porcentagem = map(solo3Value, 0, 4095, 100, 0);
       solo4Porcentagem = map(solo4Value, 0, 4095, 100, 0);
 
-      temperature = convertToTemperature(tempValue);
-
-      temperatureStr = String(temperature);
-      solo1PorcentagemStr = String(solo1Porcentagem);
-      solo2PorcentagemStr = String(solo2Porcentagem);
-      solo3PorcentagemStr = String(solo3Porcentagem);
-      solo4PorcentagemStr = String(solo4Porcentagem);
+     
+      client.publish(topic1, String(humi).c_str());
+      client.publish(topic2, String(solo1Porcentagem).c_str());
+      client.publish(topic3, String(solo2Porcentagem).c_str());
+      client.publish(topic4, String(solo3Porcentagem).c_str());
+      client.publish(topic5, String(solo4Porcentagem).c_str());
+      client.publish(topic6, String(temp).c_str());
       
-      client.publish(topic2, solo1PorcentagemStr.c_str());
-      Serial.println(solo1PorcentagemStr.c_str());
-      client.publish(topic3, solo2PorcentagemStr.c_str());
-      Serial.println(solo2PorcentagemStr.c_str());
-      client.publish(topic4, solo3PorcentagemStr.c_str());
-      Serial.println(solo3PorcentagemStr.c_str());
-      client.publish(topic5, solo4PorcentagemStr.c_str());
-      Serial.println(solo4PorcentagemStr.c_str());
-      client.publish(topic6, temperatureStr.c_str());
-      Serial.println(temperatureStr.c_str());
 
       vTaskDelay(1000);
   }
@@ -174,15 +141,13 @@ void mqttTask(void *parameter) {
 
 void setup() {
   Serial.begin(115200);
-  //pinMode(led, OUTPUT);
-  //pinMode(led2, OUTPUT);
-  //pinMode(ldr, INPUT);
+  
   pinMode(solo1, INPUT);
   pinMode(solo2, INPUT);
   pinMode(solo3, INPUT);
   pinMode(solo4, INPUT);
-  pinMode(temp, INPUT);
-  //pinMode(cooler, OUTPUT);
+
+  dht.begin();
 
   xSemaphore = xSemaphoreCreateMutex();
 
@@ -203,14 +168,6 @@ void setup() {
     1,
     NULL,
     0);
-  xTaskCreatePinnedToCore(
-    ledTask,
-    "LedTask",
-    10000,
-    NULL,
-    1,
-    NULL,
-    1);
 }
 
 void loop() {
